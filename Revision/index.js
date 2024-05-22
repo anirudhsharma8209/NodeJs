34551a0ec2076d24743e7ea9ba66883acb4f0b8c
anirudhsharma8209 <devprakashsharma7790@gmail.com>
2024-05-22 10:47:41 +0530

    Adding the multiple pages of ejs form and table

--- Revision/index.js
+++ Revision/index.js
@@ -3,9 +3,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get("/", (req, res) => {
+    res.render('index');
+})
+
+app.get("/formdata", (req, res) => {
+    let userData = req.query;
+    res.render('table', userData);
+})
+
+app.listen(5000, () => {
+    console.log("Your server is running in http://localhost:5000");
 });
-
-
-app.listen(5000);
